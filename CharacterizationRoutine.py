d487b7f872da1f36757a0d03cca64563c929f674
burrzach <61149333+burrzach@users.noreply.github.com>
2023-10-26 10:53:22 +0200

    Update CharacterizationRoutine.py
    Better fitting of planet position

--- CharacterizationRoutine.py
+++ CharacterizationRoutine.py
@@ -3,8 +3,10 @@ import numpy as np
 import math
 from pynpoint import Pypeline, FitsReadingModule, ParangReadingModule, WavelengthReadingModule,\
     AddFramesModule, RemoveFramesModule, FalsePositiveModule, AperturePhotometryModule, \
-    DerotateAndStackModule, FitCenterModule, ShiftImagesModule, TextWritingModule
+    DerotateAndStackModule, FitCenterModule, ShiftImagesModule, TextWritingModule, \
+    FakePlanetModule, PSFpreparationModule, AddLinesModule
 from pynpoint.core.processing import ProcessingModule
+from pynpoint.util.image import cartesian_to_polar, center_subpixel
 import configparser
 
 
@@ -15,6 +17,7 @@ obs = '2023-07-26-1'
 pos_guess = [(109., 58.)] #2023-07-26-1
 offset = 5
 radius = 0.025
+scale = 1.73 / 290
 
 folder = "/data/zburr/yses_ifu/2nd_epoch/processed/"+obs+"/products/"
 
@@ -23,7 +26,7 @@ folder = "/data/zburr/yses_ifu/2nd_epoch/processed/"+obs+"/products/"
 config = configparser.ConfigParser()
 config.add_section('header')
 config.add_section('settings')
-config['settings']['PIXSCALE'] = str(1.73/290)
+config['settings']['PIXSCALE'] = str(scale)
 
 with open(folder+'PynPoint_config.ini', 'w') as configfile:
     config.write(configfile)
@@ -205,6 +208,22 @@ module = ReshapeModule(name_in='shape_down_psf',
 pipeline.add_module(module)
 pipeline.run_module('shape_down_psf')
 
+#prepare psf for injecting as fake planet
+module = PSFpreparationModule(name_in='maskpsf', 
+                              image_in_tag='psf_coadd', 
+                              image_out_tag='planet',
+                              cent_size=None,
+                              edge_size=0.1)
+pipeline.add_module(module)
+pipeline.run_module('maskpsf')
+
+module = AddLinesModule(name_in='pad', 
+                        image_in_tag='planet', 
+                        image_out_tag='psf_resize', 
+                        lines=(105,105,105,105))
+pipeline.add_module(module)
+pipeline.run_module('pad')
+
 
 ## Measure star spectrum ##
 spectra = np.zeros((39, 2+len(pos_guess)))
@@ -222,43 +241,71 @@ spectra[:,1] = pipeline.get_data('star_phot')[:,0]
 star_tot = sum(spectra[2:-2,1])
 
 
-## Find planet position ## #!!!
-module = FitCenterModule(name_in='fit',
-                         image_in_tag='science_coadd',
-                         fit_out_tag='science_centering',
-                         mask_radii=(None,0.85),
-                         sign='positive',
-                         model='gaussian',
-                         filter_size=None)
-pipeline.add_module(module)
-pipeline.run_module('fit')
-
 
 ## Loop for multiple companions ##
-data = np.full((5,2+len(pos_guess)), np.nan)
+data = np.full((6,2+len(pos_guess)), np.nan)
 for i, guess in enumerate(pos_guess):
-    #find position
+    #remove all other planets
+    science_image = 'science_coadd'
+    image_out = 'removed1'
+    count = 1
+    for j, guess2 in enumerate(pos_guess):
+        if j != i:
+            count += 1
+            module = FakePlanetModule(name_in='fake',
+                                      image_in_tag=science_image, 
+                                      psf_in_tag='planet', 
+                                      image_out_tag=image_out, 
+                                      position=guess2, 
+                                      magnitude=0.)
+            pipeline.add_module(module)
+            pipeline.run_module('fake')
+            
+            science_image = image_out
+            image_out = image_out[:-1] + str(count)
+            
+            
+    #find planet position
+    module = FitCenterModule(name_in='fit',
+                             image_in_tag=science_image,
+                             fit_out_tag='companion_pos',
+                             mask_radii=(None,0.85),
+                             sign='positive',
+                             model='gaussian',
+                             filter_size=None)
+    pipeline.add_module(module)
+    pipeline.run_module('fit')
+
+    pic = pipeline.get_data('science_coadd')
+    center = center_subpixel(pic)
+
+    comp_fit = pipeline.get_data('companion_pos')
+    pos_pix = (comp_fit[0]+center[0], comp_fit[2]+center[1])
+    pos_pol = np.array(cartesian_to_polar(center, pos_pix[1], pos_pix[0]))
+    pos_pol[0] *= scale
+
+    err1 = np.array(cartesian_to_polar(center, pos_pix[1]-comp_fit[3], pos_pix[0]-comp_fit[1]))
+    err2 = np.array(cartesian_to_polar(center, pos_pix[1]+comp_fit[3], pos_pix[0]+comp_fit[1]))
+    pos_err = abs(err1 - err2)
+    
+    data[0, i+2] = pos_pol[0] #sep
+    data[1, i+2] = pos_err[0] #sep error
+    data[2, i+2] = pos_pol[1] #angle
+    data[3, i+2] = pos_err[1] #angle error
+    
+    
+    #measure snr
     module = FalsePositiveModule(name_in='find_companion',
                                  image_in_tag='science_coadd',
                                  snr_out_tag='companion_snr', 
-                                 position=guess,
-                                 aperture=radius,
-                                 optimize=True,
-                                 tolerance=0.01,
-                                 offset=offset)
+                                 position=pos_pix,
+                                 aperture=radius)
     pipeline.add_module(module)
     pipeline.run_module('find_companion')
     
     snr = pipeline.get_data('companion_snr')[0]
-    # print('x position (pix), y position (pix), separation (arcsec), position angle (deg), SNR, FPF')
-    # print(snr)
-    pos_pix = (snr[0], snr[1]) #position in pixels
-    pos_pix = guess #!!!
-    
-    data[0, i+2] = snr[2] #sep
-    data[1, i+2] = snr[3] #angle
-    data[3, i+2] = snr[4] #snr
-    data[4, i+2] = snr[5] #fpf
+    data[4, i+2] = snr[4] #snr
+    data[5, i+2] = snr[5] #fpf
     
     
     #measure companion spectrum
@@ -273,7 +320,7 @@ for i, guess in enumerate(pos_guess):
     spectra[:,i+2] = pipeline.get_data('companion_phot')[:,0]
     
     
-    #measure 
+    #calculate mag
     companion_tot = sum(spectra[2:-2,i+2])
     try:
         data[2, i+2] = -2.5*math.log10(companion_tot/star_tot)
@@ -283,16 +330,17 @@ for i, guess in enumerate(pos_guess):
 
 ## Format and output data ##
 data = np.vstack((data,spectra))
-np.savetxt(folder+obs+'_companion_data.txt', data)
+np.savetxt(folder+obs+'_companion_data.txt', data, 
+           header='# columns: wl,star,companions;\n# rows: sep,sep_err,angle,angle_err,snr,fpf,d_mag')
 
-module = TextWritingModule(name_in='write_centering', 
-                           data_tag='science_centering', 
-                           file_name=folder+obs+'_centering_data.txt',
-                           header='#x offset (pix), x offset err (pix),'+\
-                                  ' y offset (pix), y offset err (pix),'+\
-                                  ' FWHM major axis (arcsec), FWHM major axis err (arcsec),'+\
-                                  ' FWHM minor axis (arcsec), FWHM minor axis err (arcsec),'+\
-                                  ' amp (ADU), amp err (ADU), angle (deg), angle err (deg),'+\
-                                  ' offset (ADU), offset err (ADU)')
-pipeline.add_module(module)
-pipeline.run_module('write_centering')
+# module = TextWritingModule(name_in='write_centering', 
+#                            data_tag='science_centering', 
+#                            file_name=folder+obs+'_centering_data.txt',
+#                            header='#x offset (pix), x offset err (pix),'+\
+#                                   ' y offset (pix), y offset err (pix),'+\
+#                                   ' FWHM major axis (arcsec), FWHM major axis err (arcsec),'+\
+#                                   ' FWHM minor axis (arcsec), FWHM minor axis err (arcsec),'+\
+#                                   ' amp (ADU), amp err (ADU), angle (deg), angle err (deg),'+\
+#                                   ' offset (ADU), offset err (ADU)')
+# pipeline.add_module(module)
+# pipeline.run_module('write_centering')
