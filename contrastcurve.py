0969d92f202918cd676d6e43f19b1c4b7ba0a637
burrzach <61149333+burrzach@users.noreply.github.com>
2024-01-05 15:32:32 +0100

    Increase bounds for optimization

--- contrastcurve.py
+++ contrastcurve.py
@@ -312,7 +312,7 @@ for i, sep in enumerate(sep_space):
         #before subtraction
         res = root_scalar(PlanetInjection, 
                           args=(pipeline, science_image, sep, angle, pos_pix, threshold, False),
-                          bracket=(0.,10.),
+                          bracket=(0.,15.),
                           x0=initial_guess_pre,
                           rtol=tolerance,
                           maxiter=iterations)
@@ -324,7 +324,7 @@ for i, sep in enumerate(sep_space):
         #after subtraction
         res = root_scalar(PlanetInjection, 
                           args=(pipeline, science_image, sep, angle, pos_pix, threshold, True),
-                          bracket=(initial_guess_pre - 2., 16.),
+                          bracket=(initial_guess_pre - 4., 20.),
                           x0=initial_guess_post,
                           rtol=tolerance,
                           maxiter=iterations)
